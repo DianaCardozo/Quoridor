230974d962a79cc324f64b84cc2e89eb77a03fd8
DianaCardozo <125745016+DianaCardozo@users.noreply.github.com>
2024-06-01 00:05:54 -0500

    Update modal.vue
    Se creo el modal-instrucciones

--- src/organismos/modal.vue
+++ src/organismos/modal.vue
@@ -20,16 +20,33 @@
   <template>
     <div class="modal" v-if="MostrarModal">
       <div class="modal-content">
-        <p>¡Bien hecho Jugador {{ ganador }}!</p>
-        <button class="modal-content__reset-button" @click="resetGame">Volver a jugar</button>
+        <p>¡Ganaste jugador {{ ganador }}!</p>
+        <button class="modal-content__reset-button" @click="resetGame">Reiniciar</button>
       </div>
     </div>
+
+    <div class="modal-instrucciones">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2>Instrucciones del Juego</h2>
+        </div>
+        <div class="modal-body">
+          <p>1. Cada jugador tiene una ficha. El jugador 1 tiene la ficha morada oscura y el jugador 2 tiene la ficha morada suave.</p>
+          <p>2. Los jugadores se turnan para mover sus fichas. Solo puedes mover tu ficha a celdas adyacentes vacías.</p>
+          <p>3. El objetivo del juego es llegar a la fila del oponente con tu ficha.</p>
+          <p>4. Puedes bloquear celdas para dificultar el movimiento de tu oponente.</p>
+          <p>5. El juego termina cuando un jugador alcanza la fila de inicio del oponente o cuando no hay más movimientos posibles.</p>
+        </div>
+      </div>
+    </div>
+  
+
   </template>
   
   <style scoped>
 
-Bloque
-/* .modal {
+/* Bloque */
+.modal {
   position: fixed;
   top: 0;
   left: 0;
@@ -40,21 +57,8 @@ Bloque
   display: flex;
   justify-content: center;
   align-items: center;
-} */
-.modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  background-color: rgba(255, 255, 255, 0.8); /* Blanco semitransparente */
-  display: flex;
-  justify-content: center;
-  align-items: center;
 }
 
-/* 
 .modal-content {
   background-color: #1a1a1d;
   padding: 20px;
@@ -68,23 +72,10 @@ Bloque
   color: white;
   font-family: 'Arial', sans-serif;
   animation: fadeIn 0.5s;
-} */
-
-.modal-content {
-  background-color: #354f52; /* Verde oscuro */
-  padding: 30px; /* Aumentado el espaciado interno */
-  border: 2px solid #7d0396; /* Color del borde */
-  border-radius: 15px; /* Radio del borde */
-  width: 90%; /* Ancho */
-  /* max-width: 800px; /* Ancho máximo */
-  /* max-height: 400px; Altura máxima */ 
-  text-align: center;
-  color: white; /* Color del texto */
-  font-family: 'Arial', sans-serif;
-  animation: fadeIn 0.5s;
 }
+
 /* Elemento */
-/* .modal-content__reset-button {
+.modal-content__reset-button {
   background-color: #c3073f;
   color: white;
   padding: 10px 20px;
@@ -93,25 +84,13 @@ Bloque
   font-size: 16px;
   cursor: pointer;
   transition: background-color 0.3s;
-} */
-.modal-content__reset-button {
-  background-color: #a7508d; /* Verde azulado */
-  color: white; /* Color del texto del botón */
-  padding: 15px 30px; /* Tamaño del botón */
-  border: none;
-  border-radius: 8px; /* Radio del borde del botón */
-  font-size: 18px; /* Tamaño del texto del botón */
-  cursor: pointer;
-  transition: background-color 0.3s;
 }
 
 /* Modificador */
-/* .modal-content__reset-button--hover {
-  background-color: #ff6f61;
-} */
 .modal-content__reset-button--hover {
-  background-color: #6ab9bf; /* Color del botón al pasar el ratón */
+  background-color: #ff6f61;
 }
+
 /* Animation */
 @keyframes fadeIn {
   from {
